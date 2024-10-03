de3738aad20c10d2e31982c1d62b318f3d1f00f1
mkanias <131133498+mkanias@users.noreply.github.com>
2024-10-03 15:37:25 +1000

    When logged in as admin, the admin menu is displayed in the nav bar.

--- Home Page/homepage.js
+++ Home Page/homepage.js
@@ -13,4 +13,18 @@ document.addEventListener('DOMContentLoaded', function () {
         // Redirect to login page
         window.location.href = "../Login Page/loginpage.html";
     });
+
+    // Logic to show admin menu in header
+    if (localStorage.getItem('isAdminLoggedIn')) {
+        const nav = document.getElementById('nav');
+        nav.innerHTML = `
+        <a href="../Home Page/homepage.html">Home</a>
+        <a href="../Product Backlog Page/productbacklogpage.html">Product Backlog</a>
+        <a href="../Sprint Backlog Page/sprintbacklog.html">Sprint Backlog</a>
+        <span id="adminLink">
+        <a href="../Admin Page/adminmenu.html">Admin Menu</a>
+        </span>
+        <button class="logout-button" id="logoutButton">Logout</button>
+        `;
+    }
 });
